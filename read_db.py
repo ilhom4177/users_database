531ed01335697a5c0dccf2ad9af8cc053373fd47
ZARIFMAC <uzbzarif@gmail.com>
2023-01-27 10:25:47 +0500

    logical and queary

--- read_db.py
+++ read_db.py
@@ -1,13 +1,15 @@
-from tinydb import TinyDB
-
+from tinydb import TinyDB,Query
+from pprint import pprint
 db = TinyDB('db.json',indent=4)
 # Read the entire database
 data = db.all()
 # Count the female users
 count = 0 
+user = Query()
 
-for row in data:
-    if row['gender']=='Female':
-        count += 1
+female_users = db.search((user['gender']=='Female') & (user['country']=='China'))
 
-print(count)
+
+pprint(female_users)
+print(len(female_users))
+
