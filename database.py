6d3d9b02661932d98f7199ea29b2f147e98983a5
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-06 19:44:04 +0500

    remove field

--- database.py
+++ database.py
@@ -1,22 +1,8 @@
 from tinydb import TinyDB, Query
+from tinydb.operations import delete
 
 db = TinyDB('db.json', indent=4, separators=(',', ': '))
 users = db.table('users')
 
 
-def get_users() -> list:
-    '''Returns a list of users from the database.'''
-    return users.all()
-
-def check(cond=None, doc_id=None):
-    return users.contains(doc_id=doc_id)
-
-def get_user_by_id(id: int) -> dict:
-    '''Returns a user by their ID.'''
-    pass
-
-def get_user_by_full_name(first_name: str, last_name: str) -> dict:
-    '''Returns a user by their full name.'''
-    pass
-
-print(check(doc_id=1000))
+users.remove(delete('id'))
