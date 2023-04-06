4cc49d8ce9526bbe85460c323c0fce9aa9b999bb
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-06 19:23:36 +0500

    check by doc_id

--- database.py
+++ database.py
@@ -8,6 +8,9 @@ def get_users() -> list:
     '''Returns a list of users from the database.'''
     return users.all()
 
+def check(cond=None, doc_id=None):
+    return users.contains(doc_id=doc_id)
+
 def get_user_by_id(id: int) -> dict:
     '''Returns a user by their ID.'''
     pass
@@ -16,7 +19,4 @@ def get_user_by_full_name(first_name: str, last_name: str) -> dict:
     '''Returns a user by their full name.'''
     pass
 
-
-list_users = get_users()
-last = list_users[-1]
-print(last.doc_id)
+print(check(doc_id=1000))
