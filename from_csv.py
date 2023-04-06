64c269dbe2fef02f2f6e4bcef83d6946df0c4e06
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-06 18:23:57 +0500

    get usrs data

--- from_csv.py
+++ from_csv.py
@@ -6,13 +6,24 @@ users = db.table('users')
 
 def read_csv(filename: str) -> list[dict]:
     '''Reads a CSV file and returns a list of dictionaries.'''
-    pass
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        return list(reader)
+
+
+users = read_csv('users.csv')
+for user in users:
+    print(user['id'])
+
+
+
+
 
 def insert_data(data: list) -> None:
     '''Inserts data into the database.'''
     pass
 
 
-if __name__ == '__main__':
-    data = read_csv('users.csv')
-    insert_data(data)
+# if __name__ == '__main__':
+#     data = read_csv('users.csv')
+#     insert_data(data)
